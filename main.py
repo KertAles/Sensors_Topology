2d6251fc7ae0dd1dad08f767d14c8f43554fa2a2
Neki Nekaj <ales.kert@gmail.com>
2023-01-05 14:56:05 +0100

    Added VR for r, and Cech for R

--- main.py
+++ main.py
@@ -1,86 +1,121 @@
 import gudhi
-from utils import read_data
+from utils import read_data, distance, numberOfComponents, powerset
 import numpy as np
 
+import math
+import miniball
 
-def findComponents(V, E):
-    components = []
+# Get Čech complex
+def cech(S, epsilon) :
+    # Get all edges using VR
+    vertices, edges = VR_edges_only(S, epsilon*2)
 
+    sx_dict = {}
+    sx_dict[0] = vertices
+    sx_dict[1] = edges
+    unchecked_vert = []
+
+    # Make adjacency list
     G = {}
-    for vert in V:
-        G[vert] = []
-    for edge in E:
-        G[edge[0]].append(edge[1])
-        G[edge[1]].append(edge[0])
+    for vert in vertices :
+        G[vert[0]] = []
+        unchecked_vert.append(S[vert[0]])
+    for edge in edges :
+        G[edge[0]].append(S[edge[1]])
+        G[edge[1]].append(S[edge[0]])
 
-    V_search = V
+    # Go through all vertices
+    for vertex in vertices :
+        unchecked_vert.remove(S[vertex[0]])
+        relevant_vert = list(set(unchecked_vert) & set(G[vertex[0]]))
+        # Get all subsets of neighbouring vertices(that haven't been checked yet)
+        combinations = powerset(relevant_vert)
 
-    while len(V_search) > 0:
-        vert = V_search[0]
-        nu_comp = expandComponent(vert, G)
-        components.append(nu_comp)
+        for comb in combinations :
+            # Check subsets with more than 1 vertex - when we add the current vertex, it makes an edge, which is boring
+            if len(comb) > 1 :
+                # Make a set of points that include the current vertex
+                comb = list(comb)
+                comb.append(S[vertex[0]])
+                comb = np.array(comb)
+                # Get the miniball
+                _, r2 = miniball.get_bounding_ball(comb)
 
-        for c_vert in nu_comp:
-            V_search.remove(c_vert)
+                # miniball returns the square of the radius
+                if math.sqrt(r2) < epsilon :
+                    dim = len(comb) - 1
+                    if dim not in sx_dict:
+                        sx_dict[dim] = []
 
-    return components
+                    # Get the indices of the used points - coordinates were used for miniball calculation
+                    nu_sx = []
+                    for v in comb :
+                        v_t = tuple(v)
+                        nu_sx.append(S.index(v_t))
+                    nu_sx = tuple(sorted(nu_sx))
+                    if nu_sx not in sx_dict[dim]:
+                        sx_dict[dim].append(nu_sx)
 
-def expandComponent(vert, G):
-    component = []
-    component.append(vert)
+    return sx_dict
 
-    to_search = []
-    curr_vert = vert
+# Generate a VR complex with max_dim=1
+def VR_edges_only(S, epsilon) :
+    edges = []
+    vertices = []
 
-    for neighbour in G[curr_vert]:
-        to_search.append(neighbour)
+    for i, v1 in enumerate(S):
+        vertices.append((i,))
+        for j, v2 in enumerate(S[i+1:]):
+            if distance(v1, v2) < epsilon:
+                edges.append((i, j+i+1))
 
-    while len(to_search) > 0:
-        curr_vert = to_search.pop(0)
-        component.append(curr_vert)
+    return (vertices, edges)
 
-        for neighbour in G[curr_vert]:
-            if not (neighbour in component) and not (neighbour in to_search):
-                to_search.append(neighbour)
 
-    return component
+# Find the optimal parameter r, using VR complex that only generates edges.
+# The function returns the optimal r, so that the VR complex it generates is connected
 def find_optimal_r(points, start, stop, delta=1e-3) :
-    #diff = stop - start
 
-    #while diff < eps :
     for r in np.arange(start, stop, delta) :
-        rips_complex = gudhi.RipsComplex(points=points, max_edge_length=r)
-        simplex_tree = rips_complex.create_simplex_tree(max_dimension=2)
-        #result_str = 'Rips complex is of dimension ' + repr(simplex_tree.dimension()) + ' - ' + \
-        #             repr(simplex_tree.num_simplices()) + ' simplices - ' + \
-        #             repr(simplex_tree.num_vertices()) + ' vertices.'
-        #print(result_str)
-        vertices = simplex_tree.num_vertices()
-        edges = 0
+        rips_complex = VR_edges_only(points, r)
+
+        components = numberOfComponents(rips_complex[0], rips_complex[1])
+        if components  == 1:
+            print('Found connected VR complex at r = ' + str(r))
+            return r, rips_complex
+
+    print('Found no connected VR complex on given interval.')
+    return -1, None
+
+
+# Find the optimal parameter R, using the Čech complex.
+# The function returns the optimal parameter R, so that the Čech complex it generates
+# has the same Euler characteristic as a sphere. Checking if connected isn't necessary, as the R
+# is always larger than r/2 which makes a connected complex in VR( and Čech with R is a subset of VR with R/2).
+def find_optimal_R(points, start, stop, delta=1e-3) :
+
+    for R in np.arange(start, stop, delta) :
+        print(R)
+        cech_cx = cech(points, R)
+        vertices = len(cech_cx[0])
+        edges = len(cech_cx[1])
         faces = 0
-        edge_list = []
-        vert_list = []
-        for filtered_value in simplex_tree.get_filtration():
-            dim = len(filtered_value[0])
-            if dim == 1 :
-                vert_list.append(filtered_value[0][0])
-            elif dim == 2 :
-                edge_list.append(filtered_value[0])
-                edges += 1
-            elif dim == 3 :
-                faces += 1
+        if 2 in cech_cx :
+            faces = len(cech_cx[2])
 
         euler = vertices - edges + faces
+        print(str(R) + ' --- ' + str(euler))
         if euler == 2 :
-            components = len(findComponents(vert_list, edge_list))
-            if components  == 1:
-                return r, rips_complex
+            print('Found complex with Euler characteristic of a sphere at R = ' + str(R))
+            return R, cech_cx
 
     print('Found no sphere on given interval.')
     return -1, None
 
 
+
 if __name__ == '__main__':
     points = read_data('./data/sensors.txt')
 
-    find_optimal_r(points, 0.6, 0.7)
+    r, vr_cx = find_optimal_r(points, 0.25, 0.7)
+    R, cech_cx = find_optimal_R(points, r, 2.0, 0.5)
--- utils.py
+++ utils.py
@@ -1,3 +1,6 @@
+import math
+from itertools import chain, combinations
+
 def read_data(path) :
     points = []
 
@@ -7,11 +10,64 @@ def read_data(path) :
     for point in data_string.split('{') :
         if len(point) > 0 :
             point_split = point.split(',')
-            point = [float(point_split[0]), float(point_split[1]), float(point_split[2].split('}')[0])]
+            point = (float(point_split[0]), float(point_split[1]), float(point_split[2].split('}')[0]))
 
             points.append(point)
 
     return points
+
+def distance(v1, v2) :
+    return math.sqrt((v1[0] - v2[0])**2 + (v1[1] - v2[1])**2)
+
+# generates all subsets
+def powerset(iterable):
+    s = list(iterable)
+    return chain.from_iterable(combinations(s, r) for r in range(len(s)+1))
+
+def numberOfComponents(V, E) :
+    return len(findComponents(V, E))
+
+# Function used to find all components
+# If only one component found - connected graph
+def findComponents(V, E):
+    components = []
+    G = {}
+    for vert in V:
+        G[vert[0]] = []
+    for edge in E:
+        G[edge[0]].append(edge[1])
+        G[edge[1]].append(edge[0])
+
+    V_search = V
+    while len(V_search) > 0:
+        vert = V_search[0]
+        nu_comp = expandComponent(vert[0], G)
+        components.append(nu_comp)
+
+        for c_vert in nu_comp:
+            V_search.remove((c_vert,))
+
+    return components
+
+# Subfunction for findComponents
+def expandComponent(vert, G):
+    component = []
+    component.append(vert)
+    to_search = []
+    curr_vert = vert
+
+    for neighbour in G[curr_vert]:
+        to_search.append(neighbour)
+
+    while len(to_search) > 0:
+        curr_vert = to_search.pop(0)
+        component.append(curr_vert)
+        for neighbour in G[curr_vert]:
+            if not (neighbour in component) and not (neighbour in to_search):
+                to_search.append(neighbour)
+
+    return component
+
 
 
 
